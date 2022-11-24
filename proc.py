6470bc7904c308b906cf7ec0c28099b1d1b37f2f
Jeremy Yen <jeremy5189@gmail.com>
2022-11-24 18:43:30 +0800

    bug fixed

--- proc.py
+++ proc.py
@@ -22,14 +22,19 @@ def read_json(filename):
 
 def search_keyword_in_json(keyword, json_path):
     json_obj = read_json(json_path)
+    sp = json_path.split('/')
+    ver = '/'.join(json_obj['versions'][::-1])
+    found = []
+    hit = 0
+
+    if '廢止' in ver:
+        # print("Skip 廢止法規：", json_obj['title'])
+        return found, hit
+
     for article in json_obj['law_data']:
         if keyword in article.get('content', ""):
-            sp = json_path.split('/')
-            ver = '/'.join(json_obj['versions'][::-1])
-            if '廢止' in ver:
-                print("Skip 廢止法規：", json_obj['title'])
-                return
-            return {
+            hit += 1
+            found.append({
                 'category1': sp[1],
                 'category2': sp[2],
                 'title': json_obj['title'],
@@ -37,7 +42,9 @@ def search_keyword_in_json(keyword, json_path):
                 'article_no': article['rule_no'],
                 'note': article.get('note', "").replace('(', '').replace(')', ''),
                 'content': article['content'].replace('　', '')
-            }
+            })
+
+    return found, hit
 
 def write_json_to_csv(json_dict, output):
     data_file = open(output, 'w')
@@ -52,19 +59,25 @@ def write_json_to_csv(json_dict, output):
         csv_writer.writerow(data.values())
     data_file.close()
 
-
 law_files = []
 ls_dir('.', law_files)
 
 found_law = []
 keyword = sys.argv[1]
 output_dir = sys.argv[2] if len(sys.argv) > 2 else '..'
+print('----------------------------------------')
 print('Searching keyword: ', keyword)
 print('Output dir: ', output_dir)
+
+stat = {'law': 0, 'article': 0}
 for law_path in law_files:
-    found = search_keyword_in_json(keyword, law_path)
+    found, hit = search_keyword_in_json(keyword, law_path)
     if found:
-        found_law.append(found)
+        stat['law'] += 1
+        stat['article'] += hit
+        found_law += found
+
+print('Found {} laws, {} articles'.format(stat['law'], stat['article']))
 
 # print(json.dumps(found_law, indent=4, ensure_ascii=False))
 write_json_to_csv(found_law, f"{output_dir}/{keyword}.csv")
